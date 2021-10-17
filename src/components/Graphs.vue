b16d02652248185a552178ad96820c599fdb235e
Santiago Garcia <sgarciavil.29@gmail.com>
2021-10-17 10:36:15 -0500

    Init de grafica

--- src/components/Graphs.vue
+++ src/components/Graphs.vue
@@ -1,13 +1,44 @@
 <template>
-  
+  <Chart
+    :size="{ width: 500, height: 400 }"
+    :data="data"
+    :margin="margin"
+    :direction="direction">
+
+    <template #layers>
+      <Grid strokeDasharray="2,2" />
+      <Line 
+        :dataKeys="['name', 'avg']" 
+        :lineStyle="{ stroke: 'red', strokeWidth:2}"
+        :dotStyle="{ stroke: 'red', strokeWidth:5}"
+      />
+    </template>
+
+  </Chart>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { Chart, Grid, Line } from 'vue3-charts'
+import { plByMonth } from './data'
 
-}
+export default defineComponent({
+  name: 'LineChart',
+  components: { Chart, Grid, Line },
+  setup() {
+    const data = ref(plByMonth)
+    const direction = ref('horizontal')
+    const margin = ref({
+      left: 0,
+      top: 20,
+      right: 20,
+      bottom: 0
+    })
+
+    return { data, direction, margin }
+  }
+})
 </script>
 
 <style>
-
 </style>
