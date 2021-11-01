1dd866047036542cf46efeb8c2f55ca38810c476
Santiago Garcia <sgarciavil.29@gmail.com>
2021-11-01 15:09:15 -0500

    Actualizado acerca de

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div class="text-center mb-10">
+    <h1 class="text-4xl">Acerca de</h1>
+
+    <p class="my-2">Rastreador de casos Covid-19 fue creado para visiualizar los casos actuales de Colombia utilizando la api de <a href="https://covid19api.com" class="underline"> api.covid19api.com </a> </p>
+
+</div>
+
 </template>
