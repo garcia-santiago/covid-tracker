fc9ec16361d1471ca2498fcc3f248e9b89111d82
Santiago Garcia <sgarciavil.29@gmail.com>
2021-11-01 15:04:43 -0500

    Agregado Footer

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
   <div class="container mx-auto">
     <router-view/>
   </div>
+  <Footer />
 </template>
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default{
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="text-center mt-4">
+         <span>Rastreador de casos Covid-19 by <a href="https://sgarcia1.herokuapp.com/" class="font-bold underline">Santiago García</a> </span>
+    </footer>
+    <div class="text-center text-gray-500">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+</template>
